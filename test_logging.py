a51e6a28cd391236047665dfd587f76f1b5d5097
0xADB <ad.beregovoy@gmail.com>
2019-02-17 19:14:24 +0300

    printing and matching log lists

--- test_logging.py
+++ test_logging.py
@@ -3,6 +3,7 @@ from subprocess import Popen, PIPE
 import time
 import pause
 from sys import exit
+from os import walk
 
 bulk = Popen(['./bulk','2'], stdin=PIPE)
 
@@ -31,7 +32,6 @@ files = [
         , "bulk%s.log" % (start + 5)
         , "bulk%s.log" % (start + 7)
         ]
-print files
 
 stamps = range(start, start + len(commands) * interval, interval)
 pause.until(start)
@@ -41,6 +41,18 @@ for i in range(0, len(commands)):
     pause.until(stamps[i] + interval)
 bulk.stdin.close()
 
+logs = []
+for (dirpath, dirnames, filenames) in walk("./"):
+    for f in filenames:
+        if (f.endswith(".log")):
+            logs.append(f)
+    break
+
+print "Expected log files: ", files
+print "Found log files   : ", logs
+if (files != logs):
+    exit("Error: log lists mismatch")
+
 for i in range(0, len(files)):
     try:
         print "testing %s" % files[i]
@@ -48,5 +60,7 @@ for i in range(0, len(files)):
             s = f.readline()
             if (s != outputs[i]):
                 exit("Error: %s:\n\t'%s'\nvs\n\t'%s'\nexpected" % (files[i], s, outputs[i]));
+            else:
+                print s, " - ok"
     except IOError:
         exit("%s is not found" % files[i]);
