c60a04cca119147dd166ac278327a3a590b53530
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-25 17:45:53 +0000

    Deploy 0xADB/bulk to github.com/0xADB/bulk.git:gh-pages

--- search/all_3.js
+++ search/all_3.js
@@ -8,5 +8,5 @@ var searchData=
   ['commands',['commands',['../namespacetest__logging.html#a993ba825df38bb36d3bd4104da04a2e9',1,'test_logging']]],
   ['commit',['commit',['../structAccumulator.html#afb4671b43abb2c536ad43831152554a9',1,'Accumulator::commit()'],['../structConsolePrinter.html#a3aad0b4c63e2c1ee9863c27426ffb691',1,'ConsolePrinter::commit()'],['../structFilePrinter.html#a29377336ccfece0fe913e15b0187f0c6',1,'FilePrinter::commit()'],['../structIProcessor.html#a1f4c869a9774396efb6646231d9cc872',1,'IProcessor::commit()'],['../structResulter.html#a16d8ff165d52f5eb2e226b2f342cc047',1,'Resulter::commit()']]],
   ['console_5fprinter_2eh',['console_printer.h',['../console__printer_8h.html',1,'']]],
-  ['consoleprinter',['ConsolePrinter',['../structConsolePrinter.html',1,'ConsolePrinter'],['../structConsolePrinter.html#a7aa7c5e80e216b6c3265f8e43a43df4d',1,'ConsolePrinter::ConsolePrinter()']]]
+  ['consoleprinter',['ConsolePrinter',['../structConsolePrinter.html',1,'']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,5 @@
 var searchData=
 [
   ['cmake_5fminimum_5frequired',['cmake_minimum_required',['../CMakeLists_8txt.html#a98e57239c6aaf660dde57deb731ff605',1,'CMakeLists.txt']]],
-  ['commit',['commit',['../structAccumulator.html#afb4671b43abb2c536ad43831152554a9',1,'Accumulator::commit()'],['../structConsolePrinter.html#a3aad0b4c63e2c1ee9863c27426ffb691',1,'ConsolePrinter::commit()'],['../structFilePrinter.html#a29377336ccfece0fe913e15b0187f0c6',1,'FilePrinter::commit()'],['../structIProcessor.html#a1f4c869a9774396efb6646231d9cc872',1,'IProcessor::commit()'],['../structResulter.html#a16d8ff165d52f5eb2e226b2f342cc047',1,'Resulter::commit()']]],
-  ['consoleprinter',['ConsolePrinter',['../structConsolePrinter.html#a7aa7c5e80e216b6c3265f8e43a43df4d',1,'ConsolePrinter']]]
+  ['commit',['commit',['../structAccumulator.html#afb4671b43abb2c536ad43831152554a9',1,'Accumulator::commit()'],['../structConsolePrinter.html#a3aad0b4c63e2c1ee9863c27426ffb691',1,'ConsolePrinter::commit()'],['../structFilePrinter.html#a29377336ccfece0fe913e15b0187f0c6',1,'FilePrinter::commit()'],['../structIProcessor.html#a1f4c869a9774396efb6646231d9cc872',1,'IProcessor::commit()'],['../structResulter.html#a16d8ff165d52f5eb2e226b2f342cc047',1,'Resulter::commit()']]]
 ];
